2202b801257824366f93a2311209543c6a82f7b1
ForgetMeNot327 <83169406+ForgetMeNot327@users.noreply.github.com>
2022-04-14 21:53:28 +0700

    add two function

--- dunia.js
+++ dunia.js
@@ -1,2 +1,10 @@
 //saya adalah calon programer yang akan menguasai dunia
 console.log("hello world");
+function main(callback){
+  return callback();
+}
+function randomNumber(){
+ console.log('nomor acak wkwk'); 
+}
+
+const random = main(randomNumber);
